2d6fb0d73eae6cb88ba08ee8b5a5193157f481b3
pianist-in-tech <117665726+pianist-in-tech@users.noreply.github.com>
2023-01-17 11:43:12 -0700

    Update Timers Exercise.js

--- Timers Exercise.js
+++ Timers Exercise.js
@@ -18,16 +18,3 @@ function countDown(time){
 
   },1000)
 }
-
-function randomGame(){
-  let num;
-  let times = 0;
-  let timer = setInterval(function(){
-    num = Math.random();
-    times++
-    if(num > .75) {
-      clearInterval(timer);
-      console.log("It took " + times + " try/tries.");
-    }
-  },1000)
-}
