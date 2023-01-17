b893ad7ee38f04ca4ca57676dca3a32b048b72f7
pianist-in-tech <117665726+pianist-in-tech@users.noreply.github.com>
2023-01-17 11:38:36 -0700

    Add files via upload

---
+++ Timers Exercise.js
@@ -0,0 +1,33 @@
+Springboard
+
+Timers Exercise
+
+« Back to Homepage
+
+Timers Exercise
+function countDown(time){
+  let timer = setInterval(function(){
+    time--;
+    if(time <= 0){
+      clearInterval(timer);
+      console.log('DONE!');
+    }
+    else {
+      console.log(time);
+    }
+
+  },1000)
+}
+
+function randomGame(){
+  let num;
+  let times = 0;
+  let timer = setInterval(function(){
+    num = Math.random();
+    times++
+    if(num > .75) {
+      clearInterval(timer);
+      console.log("It took " + times + " try/tries.");
+    }
+  },1000)
+}
